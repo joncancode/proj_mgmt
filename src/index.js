b5bee9ee3acf30b0d55ab0a4a83adbcd5a311912
joncancode <jc.haines19@gmail.com>
2018-10-07 14:27:21 -0400

    fix typo in import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
-import rootReducer from '.store/reducers/rootReducer';
+import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
