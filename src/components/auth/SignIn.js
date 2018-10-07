770e8f7590ba0fb928c271e67384e1cfc1d435b8
joncancode <jc.haines19@gmail.com>
2018-10-07 12:57:17 -0400

    add signin component

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  state = {};
+
+  handleChange = (e) => {
+      console.log(e)
+  }
+
+  handleSubmit = (e) => {
+    console.log(e)
+}
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5>sign in</h5>
+          <div className='input-field'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' onChange={this.handleChange} />
+          </div>
+          <div className='input-field'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' onChange={this.handleChange} />
+          </div>
+        <div className='input-field'>
+        <button className='btn orange'>Login</button>
+    </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
