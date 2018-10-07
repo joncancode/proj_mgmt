64634b36630b29aa4fd82e0376191241f8657e0e
joncancode <jc.haines19@gmail.com>
2018-10-07 15:07:45 -0400

    push dummy data for proj titles

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import projectReducer from '../../store/reducers/projectReducer';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
         <div className="card-content">
-          <span className="card-title">Project title</span>
+          <span className="card-title">{project.title}</span>
           <p>posted by...</p>
           <p className="grey-text">Tuesday</p>
         </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'first thing to do', content: 'blah blah'},
+        {id: '2', title: 'go there', content: 'blah blah'},
+        {id: '3', title: 'finish up', content: 'blah blah'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
