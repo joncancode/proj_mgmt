a17d5e93f33665955de5aa160dbec821b94b1cf4
joncancode <jc.haines19@gmail.com>
2018-10-08 18:47:09 -0400

    data from firebase

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,16 +1,22 @@
 const initState = {
-    projects: [
-        {id: '1', title: 'first thing to do', content: 'blah blah'},
-        {id: '2', title: 'go there', content: 'blah blah'},
-        {id: '3', title: 'finish up', content: 'blah blah'}
-    ]
-}
+  projects: [
+    { id: '1', title: 'first thing to do', content: 'blah blah' },
+    { id: '2', title: 'go there', content: 'blah blah' },
+    { id: '3', title: 'finish up', content: 'blah blah' }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
-    switch (action.type) {
-        case 'CREATE_PROJECT': console.log('created project', action.project)
-    }
-    return state
-}
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('created project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('created project error', action.error);
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default projectReducer;
